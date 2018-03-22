65e321cad1a3957cba858ab45e0cfc08bb9a45d3
AzJezz <sgmati48@gmail.com>
2018-03-22 19:44:43 +0100

    Create SilexServiceProvider.php

---
+++ src/PwnedPasswords/providers/SilexServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace PwnedPasswords;
+
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+
+class SilexServiceProvider implements ServiceProviderInterface
+{
+    public function register(Container $app)
+    {
+        
+        $app['pwned_passwords.method'] = PwnedPasswords::CURL;
+        
+        $app['pwned_passwords.curl_options'] = [];
+        
+        $app['pwned_passwords'] = function($app) {
+            $pp = new PwnedPasswords();
+            $pp->setMethod($app['pwned_passwords.method'];
+            $pp->setCurlOptions($app['pwned_passwords.curl_options']);
+        }
+        
+    }
+}
